44bfa2cc99982b96d858142bc00d58700402b031
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2011-02-05 11:04:26 +0000

    do checks depending on how old they are
    git-svn-id: https://svn.liip.ch/repos/public/misc/liipto/trunk@15324 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- checkSpam.php
+++ checkSpam.php
@@ -2,13 +2,14 @@
 
 include("localinc/UrlCheck.php");
 
-//check( "(lastCheck = '0000-00-00 00:00:00' OR DATE_ADD(lastCheck, INTERVAL 12 HOUR) < now())");
+// neuer als 6 Stunden => alle 1 Stunden
+check( " DATE_ADD(added, INTERVAL 6 HOUR) > now() AND DATE_ADD(lastCheck, INTERVAL 59 MINUTE) < now() ");
 // neuer als 2 Tage => alle 2 Stunden
 check( " DATE_ADD(added, INTERVAL 2 DAY) > now() AND DATE_ADD(lastCheck, INTERVAL 2 HOUR) < now() ");
 // neuer als 7 Tage => alle 4 Stunden
 check( " DATE_ADD(added, INTERVAL 7 DAY) > now() AND DATE_ADD(lastCheck, INTERVAL 4 HOUR) < now() ");
-// neuer als 21 Tage => alle 8 Stunden
-check( " DATE_ADD(added, INTERVAL 21 DAY) > now() AND DATE_ADD(lastCheck, INTERVAL 8 HOUR) < now() ");
+// neuer als 21 Tage => alle 12 Stunden
+check( " DATE_ADD(added, INTERVAL 21 DAY) > now() AND DATE_ADD(lastCheck, INTERVAL 12 HOUR) < now() ");
 // neuer als 60 Tage => alle 24 Stunden
 check( " DATE_ADD(added, INTERVAL 60 DAY) > now() AND DATE_ADD(lastCheck, INTERVAL 24 HOUR) < now() ");
 // neuer als 180 Tage => alle 4 Tage
@@ -16,12 +17,12 @@ check( " DATE_ADD(added, INTERVAL 180 DAY) > now() AND DATE_ADD(lastCheck, INTER
 // neuer als 365 Tage => alle  14 Tage 
 check( " DATE_ADD(added, INTERVAL 365 DAY) > now() AND DATE_ADD(lastCheck, INTERVAL 14 DAY) < now() ");
 // neuer als 700 Tage => alle  28 Tage 
-check( " DATE_ADD(added, INTERVAL 365 DAY) > now() AND DATE_ADD(lastCheck, INTERVAL 28 DAY) < now() ");
+check( " DATE_ADD(added, INTERVAL 700 DAY) > now() AND DATE_ADD(lastCheck, INTERVAL 28 DAY) < now() ");
 
 function check($where) {
    	$c = new UrlCheck();
 $dbh = new PDO('mysql:host=localhost;dbname=liip_to', "liip_to", "oP3phuki");
-foreach ($dbh->query("SELECT * from urls WHERE lastCheck = '0000-00-00 00:00:00' OR (  $where ) order by RAND() LIMIT 100; ") as $row) {
+foreach ($dbh->query("SELECT * from urls WHERE lastCheck = '0000-00-00 00:00:00' OR (  $where ) order by RAND() LIMIT 50; ") as $row) {
 $reason = array();
 print $row['url'] ."\n";
 	if(   $c->isListed($row['url'],null,true)) {
