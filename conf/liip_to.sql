8bc696d4583df5b1a7408f25c31eba0a85ec4ee8
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2011-02-18 21:10:58 +0000

    new columns
    git-svn-id: https://svn.liip.ch/repos/public/misc/liipto/trunk@15367 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- conf/liip_to.sql
+++ conf/liip_to.sql
@@ -1,48 +1,75 @@
--- phpMyAdmin SQL Dump
--- version 2.9.1.1-Debian-3
--- http://www.phpmyadmin.net
--- 
--- Host: localhost
--- Generation Time: Jul 23, 2008 at 02:31 PM
--- Server version: 5.0.32
--- PHP Version: 5.2.6-dev
--- 
--- Database: `liip_to`
--- 
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: liip_to
+-- ------------------------------------------------------
+-- Server version	5.0.51a-24+lenny5
 
--- --------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- 
+--
 -- Table structure for table `ids_lower`
--- 
+--
 
+DROP TABLE IF EXISTS `ids_lower`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `ids_lower` (
   `id` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
--- --------------------------------------------------------
-
--- 
+--
 -- Table structure for table `ids_mixed`
--- 
+--
 
+DROP TABLE IF EXISTS `ids_mixed`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `ids_mixed` (
   `id` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+SET character_set_client = @saved_cs_client;
 
--- --------------------------------------------------------
-
--- 
+--
 -- Table structure for table `urls`
--- 
+--
 
+DROP TABLE IF EXISTS `urls`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `urls` (
   `code` varchar(30) character set latin1 collate latin1_bin NOT NULL,
   `url` varchar(2000) NOT NULL,
   `md5` char(32) NOT NULL,
   `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `added` datetime NOT NULL,
+  `lastCheck` datetime NOT NULL,
+  `IP` varchar(15) NOT NULL,
+  `spamreason` text NOT NULL,
+  `httperror` text NOT NULL,
   PRIMARY KEY  (`code`),
   UNIQUE KEY `md5` (`md5`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-02-18 21:10:27
